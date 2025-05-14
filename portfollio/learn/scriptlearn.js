a9b99fb8255b73e0d4d3ef2af2e51007fdf33a77
Prajod Krishna <142612622+Dojarp@users.noreply.github.com>
2025-05-14 09:18:06 +0530

    Delete portfollio directory

--- portfollio/learn/scriptlearn.js
+++
@@ -1,4 +0,0 @@
-function greet(){
-    const name = document.getElementById('namealert');
-    alert("Hello, " + name.value + "!");
-}
--- portfollio/script.js
+++
@@ -1,3 +0,0 @@
-function subAlert(){
-    alert("Submitted Sucessfully!");
-}
